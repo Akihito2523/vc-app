2598a8784eae3f257118a3ff143f462ff84d069a
Akihito2523 <akihito0141@gmail.com>
2022-08-26 18:21:08 +0900

    Rubyを追加

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class LanguageController extends Controller {
     public function index() {
-        $language = ['HTML', 'CSS', 'Javascript'];
+        $language = ['HTML', 'CSS', 'Javascript', 'PHP', 'Ruby'];
         $aaa = '辛い料理';
         return view('language.index', ['language' => $language, 'aaa' => $aaa]);
     }
