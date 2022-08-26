66174ef0125db94ed628c5d8b6daaace9fcff497
Akihito2523 <akihito0141@gmail.com>
2022-08-26 13:31:45 +0900

    ルーティングからViewを表示

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//ルーティングから直接Viewに表示
+// Route:::Httpメソッド(URI, ACTION);
+Route::get('/route/hello', function () {
+    return '<h1>Hello from ROute!';
+});
+
+//viewを表示
+Route::get('view/hello', function () {
+    return view('message.hello');
+});
+
+//変数を直接渡す
+Route::get('view/var', function () {
+    $Variable = 'Ello from ウェブ.php';
+    return view('message.var', ['variable' => $Variable]);
+});
+
+//URLを渡す
+Route::get('view/word/{msg}', function ($msg) {
+    return view('message.word', ['msg' => $msg]);
+});
+
+//URLを複数渡す
+Route::get('view/word/{name}/{msg}', function ($name, $msg) {
+    return view('message.word2', [
+        'name' => $name,
+        'msg' => $msg
+    ]);
+});
